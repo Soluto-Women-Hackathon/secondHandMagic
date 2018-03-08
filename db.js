3dc9b0f67a662220509e3b311b290f3c3243834a
Lena Kaplan <lenak@traxretail.com>
2018-03-08 22:57:26 +0200

    move db to seprate file

---
+++ db.js
@@ -0,0 +1,22 @@
+module.exports = [{
+    "name": "toy car",
+    "user" : {
+        "user_name": "bob",
+        "uesr_email": "bob@gmail.com"
+    },
+    "age": "12M",
+    "gender": null,
+    "image_path":"./img001.jpg",
+    "category": "toys"
+},
+    {
+        "name": "red dress",
+        "user" : {
+            "user_name": "bob",
+            "uesr_email": "bob@gmail.com"
+        },
+        "age": "12M",
+        "gender": "girl",
+        "image_path":"./img001.jpg",
+        "category": "cloths"
+    }];
--- logic.js
+++ logic.js
@@ -1,25 +1,4 @@
-const products = [{
-    "name": "toy car",
-    "user" : {
-    "user_name": "bob",
-        "uesr_email": "bob@gmail.com"
-},
-    "age": "12M",
-    "gender": null,
-    "image_path":"./img001.jpg",
-    "category": "toys"
-},
-    {
-    "name": "red dress",
-    "user" : {
-    "user_name": "bob",
-        "uesr_email": "bob@gmail.com"
-},
-    "age": "12M",
-    "gender": "girl",
-    "image_path":"./img001.jpg",
-        "category": "cloths"
-}];
+const products = require('./db');
 
 module.exports = {
     getProductByCategory: function (category, done) {
